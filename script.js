eeab5a1942b5e3bf768ce0f1728aaee73d9e5623
josecanaya <josecanaya1889@gmail.com>
2025-07-27 18:14:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
      CONFIG & BARRA DE PROGRESO
   ============================ */
   const META = 1_000_000;
-  let recaudado = 50_000;
+  let recaudado = 98_000;
 
   const barra = document.getElementById("barra");
   const recaudadoTexto = document.getElementById("recaudado");
